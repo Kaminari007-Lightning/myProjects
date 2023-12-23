692bb82624326a758c8a2ad6a44bce83371c3011
Kaminari <gamerred108@gmail.com>
2023-12-23 21:32:53 +0100

    some modificted things

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
 int main()
 {
     cout << "hello" << endl;
+    cout << "Press Any Key To Continue..";
+    getch();
     return 0;
 }
