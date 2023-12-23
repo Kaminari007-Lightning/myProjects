ed9b2c96f370ab5169434f7fd88dacfa685b2827
Kaminari <gamerred108@gmail.com>
2023-12-23 15:31:26 +0100

    ani badalt shwya

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
 int main()
 {
     cout << "Testing";//testing
+    getch();
+    cout << "press key to continue";
     return 0;
 }
