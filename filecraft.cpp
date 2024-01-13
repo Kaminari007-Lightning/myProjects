be23b0250626c5293024cd3222c4ebfc53724334
Kaminari <gamerred108@gmail.com>
2024-01-13 10:17:28 +0100

    i add an write funtion

--- filecraft.cpp
+++ filecraft.cpp
@@ -4,8 +4,21 @@
 
 using namespace std;
 
+#define STRING_TEXT "data.txt"
+
+void addtofile(string text, string filePath){
+    ofstream myfile;
+    myfile.open(filePath , ios::out | ios::app);
+    myfile<< text + "\n";
+    myfile.close();
+}
+
 int main(){
-    
+    string text;
+    while(true){
+        getline(cin, text);
+        addtofile(text, STRING_TEXT);
+    }
 
     system("pause");
 
