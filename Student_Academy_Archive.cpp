b560d5acc6a8db42b6cb9c0b44a58090a9904fb5
Kaminari <gamerred108@gmail.com>
2024-01-17 16:43:50 +0100

    i finish the project finely

--- Student_Academy_Archive.cpp
+++ Student_Academy_Archive.cpp
@@ -6,8 +6,8 @@
 #include <unistd.h>
 #include <list>
 using namespace std;
-#define FILE_PATH_BIN "Students.bin"
-#define FILE_PATH_RECORD "Record.bin"
+#define FILE_PATH_BIN "Students.txt"
+#define FILE_PATH_RECORD "Record.txt"
 class Student{
     public:
     string name;
@@ -26,50 +26,50 @@ void singleStudent(int);
 void updateInfo(int);
 void result(int);
 void deleteStudent(int);
+void destruction();
 void self_exit();
 void MainMenu();
 int main() {
-    // intro();
+    intro();
     MainMenu();
 }
-// void intro()
-// {
-//     system("color 0C");
-//     cout << "\n\n\n";
-//     Sleep(300);
-//     cout << "\t\t\t * * *  **** *      ****  ***   ***   ****   " << endl;
-//     Sleep(300);
-//     system("color 0a");
-//     cout << "\t\t\t * * * *     *     *     *   * * * * *        "<< endl;
-//     Sleep(300);
-//     system("color 0b");
-//     cout << "\t\t\t * * * ***** *     *     *   * * * * *****    " << endl;
-//     Sleep(300);
-//     system("color 0e");
-//     cout << "\t\t\t * * * *     *     *     *   * * * * *         " << endl;
-//     Sleep(300);
-//     system("color 05");
-//     cout << "\t\t\t  ***   **** *****  ****  ***  * * *  ****     " << endl;
-//     Sleep(300);
-//     system("color 0C");
-//     cout<<endl;
-//     cout<<"\t\t\t============================================="<<endl;
-//     Sleep(300);
-//     system("color 0b");
-//     cout<<"\t\t\t       THIS IS STUDENT ACADEMY ARCHIVE"<<endl;
-//     Sleep(300);
-//     system("color 0e");
-//     cout<<"\t\t\t============================================="<<endl;
-//     Sleep(300);
-//     system("color 0A");
-//     cout<<"\npress any key to continue...";
-//     cin.ignore();
-//     cin.get();
-// }
-
+void intro()
+{
+    system("color 0C");
+    cout << "\n\n\n";
+    Sleep(300);
+    cout << "\t\t\t * * *  **** *      ****  ***   ***   ****   " << endl;
+    Sleep(300);
+    system("color 0a");
+    cout << "\t\t\t * * * *     *     *     *   * * * * *        "<< endl;
+    Sleep(300);
+    system("color 0b");
+    cout << "\t\t\t * * * ***** *     *     *   * * * * *****    " << endl;
+    Sleep(300);
+    system("color 0e");
+    cout << "\t\t\t * * * *     *     *     *   * * * * *         " << endl;
+    Sleep(300);
+    system("color 05");
+    cout << "\t\t\t  ***   **** *****  ****  ***  * * *  ****     " << endl;
+    Sleep(300);
+    system("color 0C");
+    cout<<endl;
+    cout<<"\t\t\t============================================="<<endl;
+    Sleep(300);
+    system("color 0b");
+    cout<<"\t\t\t       THIS IS STUDENT ACADEMY ARCHIVE"<<endl;
+    Sleep(300);
+    system("color 0e");
+    cout<<"\t\t\t============================================="<<endl;
+    Sleep(300);
+    system("color 0A");
+    cout<<"\npress any key to continue...";
+    cin.ignore();
+    cin.get();
+}
 list<Student*> readbinfile(string filePath){
     list<Student*> fileStudent;
-    ifstream myfile(filePath, ios::in | ios::binary | ios::ate);
+    ifstream myfile(filePath, ios::in | ios::ate);
     if (myfile.is_open()){
         streampos size = myfile.tellg();
         char* memblock = new char[size];
@@ -91,7 +91,6 @@ list<Student*> readbinfile(string filePath){
     }
     return fileStudent;
 }
-
 void MainMenu(){
     short int choice; 
     system("cls");
@@ -111,27 +110,30 @@ void MainMenu(){
     Sleep(300);
     cout << "\n\n\t6. Delete student information";
     Sleep(300);
-    cout << "\n\n\t7. Exit";
+    cout << "\n\n\t7. Burn all files";
+    Sleep(300);
+    cout << "\n\n\t8. Exit";
     Sleep(300);
     cout << "\n\n\t > ";
     Sleep(300);
     cin >> choice;
     if (choice == 1) {
+        system("cls");
         Student him;
         cout<<"\n\n";
         cout<<"\t=============== CREATE A NEW INFO ================\n";
             cout<<"\n\tEnter Your ID Number : #";
             cin>>him.id;
-            if(him.id > 9999){cout<<"\n\tChoose A Number Dont Surpass \"9999\" ";addStudent(him, FILE_PATH_BIN);}
+            if(him.id > 9999){cout<<"\n\tChoose A Number Dont Surpass \"9999\" ";sleep(5);MainMenu();}
             cout<<"\n\tEnter Your Full Name : ";
             cin.ignore();
             getline(cin, him.name);
             cout<<"\n\tEnter Your Sex : ";
             cin>>him.sex;
-            if(him.sex == "male"){}else if(him.sex == "female"){}else{cout<<"\n\tPlease Be Realistic Whoever You Are";addStudent(him, FILE_PATH_BIN);} 
+            if(him.sex == "male"){}else if(him.sex == "female"){}else{cout<<"\n\tPlease Be Realistic Whoever You Are";sleep(5);MainMenu();} 
             cout<<"\n\tEnter Your Year Level : ";
             cin>>him.lvl;
-            if(him.lvl > 5){cout<<"\n\twhere Are You Going To. There 5 Levels In University ";sleep(5);addStudent(him, FILE_PATH_BIN);}
+            if(him.lvl > 5){cout<<"\n\twhere Are You Going To. There 5 Levels In University ";sleep(5);MainMenu();}
             cout<<"\n\tEnter Your Semaster 1 Mark : ";
             cin>>him.sem1;
             cout<<"\n\tEnter Your Semaster 2 MARK : ";
@@ -193,6 +195,8 @@ void MainMenu(){
         cin>>h;
         deleteStudent(h);
     }else if(choice == 7){
+        destruction();
+    }else if(choice == 8){
         self_exit();
     }else{
         cout<<"\n\tERROR";
@@ -208,7 +212,7 @@ void addStudent(Student him, string filePath)
     myfile.open(filePath , ios::out | ios::app | ios::binary);
     myfile.write((char*)&him,sizeof(him));
     myfile.close();
-    cout<<"\n\t=================================================="<<endl;
+    cout<<"\n\t=============== CREATE A NEW INFO ================\n";
     cout<<endl;
     cout<<"\t\tTHE FILE IS SUCCESSFULLY SAVED"<<endl;
     cout<<endl;
@@ -221,7 +225,6 @@ void addStudent(Student him, string filePath)
 void singleStudent(int s)
 {
     system("cls");
-    Student him;
     ifstream infile;
     infile.open(FILE_PATH_BIN,ios::app|ios::binary);
     if(infile.fail())
@@ -230,20 +233,21 @@ void singleStudent(int s)
         cin.ignore();
         cin.get();
     }
-    bool equality=false;
     cout<<"=========================VIEW A SINGLE STUDENT INFORMATION========================\n";
-    while(infile.read(reinterpret_cast<char*>(&him),sizeof(Student)))
-    {
-        if(him.id == s)
+    list<Student*> filelines = readbinfile(FILE_PATH_BIN);
+    bool equality=false;
+    for(auto line = filelines.begin(); line != filelines.end(); line++){
+        Student* student = *line;
+        if(student->id == s)
         {
         cout<<"\n";
-        cout<<"\tStudent ID Number : #"<<him.id<<endl;
-        cout<<"\tStudent Full Name : "<<him.name<<endl;
-        cout<<"\tStudent Sex : "<<him.sex<<endl;
-        cout<<"\tStudent Year Level : "<<him.lvl<<endl;
-        cout<<"\tSemaster 1 Mark : "<<him.sem1<<endl;
-        cout<<"\tSemaster 2 Mark : "<<him.sem2<<endl;
-        cout<<"\tSemaster 3 Mark : "<<him.sem3<<endl;
+        cout<<"\tStudent ID Number : #"<<student->id<<endl;
+        cout<<"\tStudent Full Name : "<<student->name<<endl;
+        cout<<"\tStudent Sex : "<<student->sex<<endl;
+        cout<<"\tStudent Year Level : "<<student->lvl<<endl;
+        cout<<"\tSemaster 1 Mark : "<<student->sem1<<endl;
+        cout<<"\tSemaster 2 Mark : "<<student->sem2<<endl;
+        cout<<"\tSemaster 3 Mark : "<<student->sem3<<endl;
         cout<<"\n";
         cout<<"=================================================================================="<<endl;
         equality=true;
@@ -257,7 +261,7 @@ void singleStudent(int s)
             cout<<"press any key to continue...";
             cin.ignore();
             cin.get();
-        MainMenu();   
+            MainMenu();   
 }
 
 void updateInfo(int x)
@@ -272,21 +276,20 @@ void updateInfo(int x)
         cin.ignore();
         cin.get();
     }
-     bool checker=false;
-     cout<<"\t\t============== MODIFY A STUDENT INFORMATION ==============\n\n";
-     while(!infile.eof() && checker==false)
-     {
-     infile.read(reinterpret_cast<char*>(&him),sizeof(Student));
-     {
-        if(him.id==x)
+    cout<<"\t\t============== MODIFY A STUDENT INFORMATION ==============\n\n";
+    list<Student*> filelines = readbinfile(FILE_PATH_BIN);
+    bool checker=false;
+    for(auto line = filelines.begin(); line != filelines.end(); line++){
+        Student* student = *line;
+        if(student->id==x)
         {
-            cout<<"\t\t\t\tStudent ID Number : #"<<him.id<<endl;
-            cout<<"\t\t\t\tStudent Full Name : "<<him.name<<endl;
-            cout<<"\t\t\t\tStudent Sex : "<<him.sex<<endl;
-            cout<<"\t\t\t\tStudent Year Level : "<<him.lvl<<endl;
-            cout<<"\t\t\t\tSemaster 1 Mark : "<<him.sem1<<endl;
-            cout<<"\t\t\t\tSemaster 2 Mark : "<<him.sem2<<endl;
-            cout<<"\t\t\t\tSemaster 3 Mark : "<<him.sem3<<endl<<endl<<endl;
+            cout<<"\t\t\t\tStudent ID Number : #"<<student->id<<endl;
+            cout<<"\t\t\t\tStudent Full Name : "<<student->name<<endl;
+            cout<<"\t\t\t\tStudent Sex : "<<student->sex<<endl;
+            cout<<"\t\t\t\tStudent Year Level : "<<student->lvl<<endl;
+            cout<<"\t\t\t\tSemaster 1 Mark : "<<student->sem1<<endl;
+            cout<<"\t\t\t\tSemaster 2 Mark : "<<student->sem2<<endl;
+            cout<<"\t\t\t\tSemaster 3 Mark : "<<student->sem3<<endl<<endl<<endl;
         cout<<"\tENTER THE NEW INFORMATION"<<endl;
         cout<<"============================================="<<endl;
         cout<<"What Do You Want To Change:\n1. Your ID\n2. Your Name\n3. Your Sex\n4. Your Year level\n5. Your Semaster 1 Mark\n6. Your Semaster 2 Mark\n7. Your Semaster 3 Mark";
@@ -338,9 +341,8 @@ void updateInfo(int x)
     infile.write(reinterpret_cast<char *> (&him), sizeof(Student));
     cout<<"==================================================="<<endl<<endl;
     cout<<"\t\tThe File Is Successfully Updated"<<endl;
-    checker=true;
-         }
-     }
+    checker=true;  
+        }
      }
      infile.close();
          if(checker==false)
@@ -351,11 +353,9 @@ void updateInfo(int x)
             cin.get();
         MainMenu();   
   }
-
 void result(int y)
 {
     system("cls");
-    Student him;
     ifstream infile;
     infile.open(FILE_PATH_BIN,ios::app|ios::binary);
     if(infile.fail())
@@ -364,21 +364,22 @@ void result(int y)
         cin.ignore();
         cin.get();
     }
-    bool eq=false;
     cout<<"\t===========VIEW A SINGLE STUDENT RESULT==========\n\n";
-    while(infile.read(reinterpret_cast<char*>(&him),sizeof(Student)))
-    {
-        if(him.id==y)
+    list<Student*> filelines = readbinfile(FILE_PATH_BIN);
+    bool eq=false;
+    for(auto line = filelines.begin(); line != filelines.end(); line++){
+        Student* student = *line;
+        if(student->id==y)
         {
-        cout<<"\t\tSTUDENT FULL NAME : "<<him.name<<endl;
-        cout<<"\t\tSTUDENT ID NUMBER : #"<<him.id<<endl;
-        cout<<"\t\tStudent Sex : "<<him.sex<<endl;
-        cout<<"\t\tStudent Year Level : "<<him.lvl<<endl<<endl;
-        cout<<"\t\tSUM : "<<him.sum<<endl<<endl;
-        cout<<"\t\tFINAL RESELT : "<<fixed<<setprecision(2)<<him.reselt<<endl<<endl;
-        if(him.reselt <= 10){
+        cout<<"\t\tSTUDENT FULL NAME : "<<student->name<<endl;
+        cout<<"\t\tSTUDENT ID NUMBER : #"<<student->id<<endl;
+        cout<<"\t\tStudent Sex : "<<student->sex<<endl;
+        cout<<"\t\tStudent Year Level : "<<student->lvl<<endl<<endl;
+        cout<<"\t\tSUM : "<<student->sum<<endl<<endl;
+        cout<<"\t\tFINAL RESELT : "<<fixed<<setprecision(2)<<student->reselt<<endl<<endl;
+        if(student->reselt <= 10){
             cout<<"\t\tYOU HAVE FAILED"<<endl<<endl;
-        }else if(him.reselt < 20){
+        }else if(student->reselt < 20){
             cout<<"\t\tYOU HAVE PASSED"<<endl<<endl;
         }else{
             cout<<"\t\tSOMETHING WRONG PLESSE GO CHECK YOUR INFO......."<<endl;
@@ -401,7 +402,6 @@ void result(int y)
             cin.get();
         MainMenu();   
 }
-
 void deleteStudent(int w)
 {
     system("cls");
@@ -430,13 +430,20 @@ void deleteStudent(int w)
     remove(FILE_PATH_BIN);
     rename(FILE_PATH_RECORD,FILE_PATH_BIN);
     cout<<endl;
-    cout<<"\t\t\t\tINFORMATION SUCCESSFULLY DELETED"<<endl;
+    cout<<"\t\t\t\tINFORMATION SUCCESSFULLY DELETED"<<endl<<endl;
     cout<<"press any key to continue...";
     cin.ignore();
     cin.get();
     MainMenu();
 }
-
+void destruction(){
+    remove(FILE_PATH_BIN);
+    cout << "\t\tDone !!\n"<< endl;
+    cout<< "You Are A Monster!" << endl<<endl;
+    cout<<"Have A Nice Day nigaa"<< endl<<endl;
+    sleep(7);
+    MainMenu();
+}
 void self_exit()
 {
     cout << "\n\n\n\t\tThank You For Using My Software !";
